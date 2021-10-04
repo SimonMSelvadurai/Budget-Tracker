93da32d54d820e61a2483b8b440b08791daa528e
SimonMSelvadurai <simon.selvadurai0607@gmail.com>
2021-10-05 00:18:42 +1100

    updated mongodb configs

--- server.js
+++ server.js
@@ -15,37 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect("mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useUnifiedTopology: true
-// }).then(res=>{
-//         console.log("DB Connected!")
-// }).catch(err => {
-//   console.log(Error, err.message);
-// })
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 // routes
 app.use(require("./routes/api.js"));
 
-mongoose.connect("mongodb://localhost/budget", { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true,
-}, (err, connection) => {
-if(err) {
-console.error(err)
-return
-}    
-
-console.log('Connected to DB');
-  app.listen({ port: PORT }, () => {
-      console.log(`Server running at http://localhost:${PORT}`);
-  })
-
-
-
-
-
-// app.listen(PORT, () => {
-//   console.log(`App running on port ${PORT}!`);
-// });
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
